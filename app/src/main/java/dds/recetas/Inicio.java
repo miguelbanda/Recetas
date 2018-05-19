bc556d168fb24253dccf550d41c8aa1b99e8efa9
Miguel <miguelangelbandardz@gmail.com>
2018-05-19 20:33:21 +0200

    dds

--- app/src/main/java/dds/recetas/Inicio.java
+++ app/src/main/java/dds/recetas/Inicio.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.view.Menu;
 
 public class Inicio extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,15 @@ public class Inicio extends AppCompatActivity {
         bottomNav.setOnNavigationItemSelectedListener(navListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new InicioFragment()).commit();
+
+        toolbar = findViewById(R.id.toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.topbar, menu);
+        return true;
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
