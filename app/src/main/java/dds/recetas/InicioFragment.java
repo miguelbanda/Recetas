8945ef30a65c4da6515ba2f883d9799c1ae29dd0
Miguel <miguelangelbandardz@gmail.com>
2018-06-11 21:21:20 +0200

    Version 1.0

--- app/src/main/java/dds/recetas/InicioFragment.java
+++ app/src/main/java/dds/recetas/InicioFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
